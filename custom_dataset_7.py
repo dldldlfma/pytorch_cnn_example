f4e55539ca6f595fb935b1a77f44e973220e7ab8
ksg <dldldlfma@gmail.com>
2018-11-18 18:27:44 +0900

    Add test dataset and custom_dataset_7(test_model)

--- custom_dataset_7.py
+++ custom_dataset_7.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         torchvision.transforms.Resize((64,128)),
         torchvision.transforms.ToTensor()
     ])
-    test_data = torchvision.datasets.ImageFolder(root='./origin_data', transform=trans)
+    test_data = torchvision.datasets.ImageFolder(root='./test_data', transform=trans)
 
     testloader = DataLoader(dataset=test_data, batch_size=8, shuffle=True, num_workers=4)
 
@@ -24,9 +24,26 @@ if __name__ == "__main__":
     device='cuda'
     pre_train_net = pre_train_net.to(device)
 
+    # 몇개 맞았는지 저장할 변수
+    correct = 0
+    # 전체 개수를 저장할 변수
+    total = 0
+
     for num, data in enumerate(testloader):
         inputs, labels = data
         inputs = inputs.to(device)
+
         out = pre_train_net(inputs)
-        print(out,labels)
-        break
+        _, predicted = torch.max(out,1)
+
+        #torch.Tensor.cuda()하고 torch.Tensor()는 비교가 안됩니다.
+        #따라서 .cpu() 를 이용해서 바꿔주세요
+        predicted = predicted.cpu()
+        total += labels.size(0)
+
+        #잘 맞추고 있는지 궁금하면 아래 print를 출력해 보세요
+        #print(predicted, labels)
+
+        correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 50 test images : %d %%'%(100* correct /total))
